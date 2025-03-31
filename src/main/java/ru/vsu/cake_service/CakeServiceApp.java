5008d2b16c74fbd62e6a20450ce096af56205021
Fatomartyr <choppyiney@gmail.com>
2025-04-01 00:02:06 +0300

    feat: Add CakeController. Add @SpringBootApplication annotation to entry point.

--- src/main/java/ru/vsu/cake_service/CakeServiceApp.java
+++ src/main/java/ru/vsu/cake_service/CakeServiceApp.java
@@ -1,7 +1,12 @@
 package ru.vsu.cake_service;
 
+import com.sun.tools.javac.Main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CakeServiceApp {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class);
     }
 }
