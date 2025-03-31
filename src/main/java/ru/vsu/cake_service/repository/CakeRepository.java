f1ed1ff289b4410a17e184def7dcd0502d39cf3a
Fatomartyr <choppyiney@gmail.com>
2025-03-31 21:15:25 +0300

    feat: Add CakeRepository

---
+++ src/main/java/ru/vsu/cake_service/repository/CakeRepository.java
@@ -0,0 +1,16 @@
+package ru.vsu.cake_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.vsu.cake_service.entity.Cake;
+
+import java.util.List;
+
+@Repository
+public interface CakeRepository extends JpaRepository<Cake, Long> {
+    @Query("SELECT c FROM Cake c WHERE c.name = :name")
+    List<Cake> findCakesByName(String name);
+    @Query("SELECT c FROM Cake c where c.category.id = :categoryId")
+    List<Cake> findCakesByCategory(Long categoryId);
+}
